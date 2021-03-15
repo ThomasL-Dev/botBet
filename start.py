2b4a215d542c1c6ea6f17d1be0766136cad502bb
ThomasL-Dev <58333776+ThomasL-Dev@users.noreply.github.com>
2021-03-16 00:34:21 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 from discord_handler import DiscordHandler
 
 #
-#  ODE2MDY4NjY4ODYxMzE3MTcx.YD1lLA.rJkE0SMG3nbztAA9Fb6ub2ozC20
 #
 #  https://discord.com/api/oauth2/authorize?client_id=816068668861317171&permissions=8&scope=bot
 #
